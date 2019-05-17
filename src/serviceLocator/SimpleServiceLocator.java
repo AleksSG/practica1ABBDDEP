de4922dd63a5af374d8913ec035143ba97fdc2c8
ivancolomer <31484161+ivancolomer@users.noreply.github.com>
2019-05-17 13:15:11 +0200

    XD

--- src/serviceLocator/SimpleServiceLocator.java
+++ src/serviceLocator/SimpleServiceLocator.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class SimpleServiceLocator implements ServiceLocator {
 
-    private enum ObjectType {SERVICE, CONSTANT};
+    private enum ObjectType {SERVICE, CONSTANT}
 
     private final Map<String, Pair<ObjectType, Object>> services;
 
@@ -18,16 +18,30 @@ public class SimpleServiceLocator implements ServiceLocator {
     @Override
     public void setService(String name, Factory factory) throws LocatorError {
         if(services.containsKey(name))
-            throw new LocatorError(new IllegalArgumentException("This key already exists in the map."));
+            throw new LocatorError(new IllegalArgumentException("The key already exists in the map."));
+        services.put(name, new Pair<>(ObjectType.SERVICE, factory));
     }
 
     @Override
     public void setConstant(String name, Object value) throws LocatorError {
-
+        if(services.containsKey(name))
+            throw new LocatorError(new IllegalArgumentException("The key already exists in the map."));
+        services.put(name, new Pair<>(ObjectType.CONSTANT, value));
     }
 
     @Override
     public Object getObject(String name) throws LocatorError {
-        return null;
+        Pair<ObjectType, Object> value = services.get(name);
+        if(value == null)
+            throw new LocatorError(new IllegalArgumentException("The key was not found in the map."));
+
+        switch(value.getFirst()) {
+            case SERVICE:
+                return ((Factory)value.getSecond()).create(this);
+            case CONSTANT:
+                return value.getSecond();
+        }
+
+        throw new LocatorError(new IllegalArgumentException("The ObjectType was neither SERVICE nor CONSTANT."));
     }
 }
