57b9a865eb1f9adbfb1fd6633c4955fb1b9bbf17
AleksSG <samuilov.aleks@gmail.com>
2019-05-17 12:10:32 +0200

    Structure created

---
+++ src/serviceLocator/CachedServiceLocator.java
@@ -0,0 +1,18 @@
+package serviceLocator;
+
+public class CachedServiceLocator implements ServiceLocator {
+    @Override
+    public void setService(String name, Factory factory) throws LocatorError {
+        
+    }
+
+    @Override
+    public void setConstant(String name, Object value) throws LocatorError {
+
+    }
+
+    @Override
+    public Object getObject(String name) throws LocatorError {
+        return null;
+    }
+}
