0a9921b17a361c79958cde53d06a562ba0d3d9ee
ivancolomer <31484161+ivancolomer@users.noreply.github.com>
2019-05-17 13:42:23 +0200

    XD

--- src/serviceLocator/CachedServiceLocator.java
+++ src/serviceLocator/CachedServiceLocator.java
@@ -1,18 +1,48 @@
 package serviceLocator;
 
-public class CachedServiceLocator implements ServiceLocator {
-    @Override
-    public void setService(String name, Factory factory) throws LocatorError {
+import utils.Pair;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class CachedServiceLocator implements ServiceLocator {
+
+    private enum ObjectType {SERVICE, CONSTANT}
+
+    private final Map<String, Pair<ObjectType, Object>> services;
+
+    public CachedServiceLocator(){
+        services = new HashMap<>();
     }
 
     @Override
-    public void setConstant(String name, Object value) throws LocatorError {
+    public synchronized void setService(String name, Factory factory) throws LocatorError {
+        if(services.containsKey(name))
+            throw new LocatorError(new IllegalArgumentException("The key already exists in the map."));
+        services.put(name, new Pair<>(ObjectType.SERVICE, factory));
+    }
 
+    @Override
+    public synchronized void setConstant(String name, Object value) throws LocatorError {
+        if(services.containsKey(name))
+            throw new LocatorError(new IllegalArgumentException("The key already exists in the map."));
+        services.put(name, new Pair<>(ObjectType.CONSTANT, value));
     }
 
     @Override
     public Object getObject(String name) throws LocatorError {
-        return null;
+        Pair<ObjectType, Object> value = services.get(name);
+        if(value == null)
+            throw new LocatorError(new IllegalArgumentException("The key was not found in the map."));
+
+        switch(value.getFirst()) {
+            case SERVICE:
+                services.replace(name, value, new Pair<>(ObjectType.CONSTANT, ((Factory)value.getSecond()).create(this)));
+                return getObject(name);
+            case CONSTANT:
+                return value.getSecond();
+        }
+
+        throw new LocatorError(new IllegalArgumentException("The ObjectType was neither SERVICE nor CONSTANT."));
     }
 }
